c2d46c374f6ae60ca8937bd79d8c625148aaa7bf
Neo Lee <neo.lee@gmail.com>
2026-01-21 16:57:51 +0800

    feat: left side bar panes and focus state

--- tui/Main.hs
+++ tui/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Relude
-import Brick.AttrMap (attrMap)
+import Brick.AttrMap (attrMap, attrName)
 import Brick.Main (App(..), defaultMain, halt, showFirstCursor)
 import Brick.Types (BrickEvent(..), EventM, Widget, nestEventM)
 import Brick.Widgets.Border
@@ -9,10 +9,12 @@ import Brick.Widgets.Core
 import qualified Brick.Widgets.List as L
 import Control.Monad.State.Class (get, modify)
 import qualified Data.Map.Strict as M
+import qualified Data.List as List
+import qualified Data.Vector as V
 import qualified Graphics.Vty as Vty
 import Oasis.Config
 import Oasis.Tui.State (AppState(..), Name(..), mkState)
-import Oasis.Types (Config(..))
+import Oasis.Types (Config(..), Defaults(..), Provider(..))
 
 drawUI :: AppState -> [Widget Name]
 drawUI st =
@@ -28,16 +30,32 @@ drawUI st =
   where
     leftPane =
       hLimit 28 $
-        borderWithLabel (txt "Providers") $
-          padAll 1 $
-            L.renderList drawProvider True (providerList st)
+        vBox
+          [ focusBorder (activeList st == ProviderList) $
+              borderWithLabel (txt "Providers [P]") $
+              padAll 1 $
+                L.renderList drawProvider (activeList st == ProviderList) (providerList st)
+          , focusBorder (activeList st == ModelList) $
+              borderWithLabel (txt "Models [M]") $
+              padAll 1 $
+                L.renderList drawProvider (activeList st == ModelList) (modelList st)
+          , focusBorder (activeList st == RunnerList) $
+              borderWithLabel (txt "Runners [R]") $
+              padAll 1 $
+                L.renderList drawProvider (activeList st == RunnerList) (runnerList st)
+          ]
     centerPane =
-      borderWithLabel (txt "Main") $
+      borderWithLabel (txt "Main [V]") $
         padAll 1 $
-          txt "Stage 1: layout ready"
+          vBox
+            [ txt ("Provider: " <> fromMaybe "-" (selectedProvider st))
+            , txt ("Model: " <> fromMaybe "-" (selectedModel st))
+            , txt ("Runner: " <> fromMaybe "-" (selectedRunner st))
+            , padTop (Pad 1) (txt "Stage 2: left panels interactive")
+            ]
     rightPane =
       hLimit 28 $
-        borderWithLabel (txt "Sidebar") $
+        borderWithLabel (txt "Sidebar [L/D]") $
           padAll 1 $
             txt ""
     statusBar =
@@ -54,16 +72,72 @@ appEvent (VtyEvent ev) =
   case ev of
     Vty.EvKey (Vty.KChar 'q') [] -> halt
     Vty.EvKey (Vty.KChar 'Q') [] -> halt
-    Vty.EvKey Vty.KUp [] -> modifyList L.listMoveUp
-    Vty.EvKey Vty.KDown [] -> modifyList L.listMoveDown
-    _ -> do
-      st <- get
-      (lst, _) <- nestEventM (providerList st) (L.handleListEvent ev)
-      modify (\s -> s { providerList = lst })
+    Vty.EvKey (Vty.KChar 'p') [] -> setActive ProviderList
+    Vty.EvKey (Vty.KChar 'P') [] -> setActive ProviderList
+    Vty.EvKey (Vty.KChar 'm') [] -> setActive ModelList
+    Vty.EvKey (Vty.KChar 'M') [] -> setActive ModelList
+    Vty.EvKey (Vty.KChar 'r') [] -> setActive RunnerList
+    Vty.EvKey (Vty.KChar 'R') [] -> setActive RunnerList
+    Vty.EvKey Vty.KEnter [] -> applySelection
+    _ -> handleActiveListEvent ev
 appEvent _ = pure ()
 
-modifyList :: (L.List Name Text -> L.List Name Text) -> EventM Name AppState ()
-modifyList f = modify (\s -> s { providerList = f (providerList s) })
+setActive :: Name -> EventM Name AppState ()
+setActive name = modify (\s -> s { activeList = name })
+
+handleActiveListEvent :: Vty.Event -> EventM Name AppState ()
+handleActiveListEvent ev = do
+  st <- get
+  case activeList st of
+    ProviderList -> do
+      (lst, _) <- nestEventM (providerList st) (L.handleListEvent ev)
+      modify (\s -> s { providerList = lst })
+    ModelList -> do
+      (lst, _) <- nestEventM (modelList st) (L.handleListEvent ev)
+      modify (\s -> s { modelList = lst })
+    RunnerList -> do
+      (lst, _) <- nestEventM (runnerList st) (L.handleListEvent ev)
+      modify (\s -> s { runnerList = lst })
+
+applySelection :: EventM Name AppState ()
+applySelection = do
+  st <- get
+  case activeList st of
+    ProviderList ->
+      case L.listSelectedElement (providerList st) of
+        Nothing -> pure ()
+        Just (_, providerName) -> do
+          let models = providerModels (config st) providerName
+          let modelList' = L.list ModelList (V.fromList models) 1
+          modify (\s -> s
+            { selectedProvider = Just providerName
+            , selectedModel = listToMaybe models
+            , modelList = modelList'
+            , statusText = "Selected provider: " <> providerName
+            })
+    ModelList ->
+      case L.listSelectedElement (modelList st) of
+        Nothing -> pure ()
+        Just (_, modelName) ->
+          modify (\s -> s
+            { selectedModel = Just modelName
+            , statusText = "Selected model: " <> modelName
+            })
+    RunnerList ->
+      case L.listSelectedElement (runnerList st) of
+        Nothing -> pure ()
+        Just (_, runnerName) ->
+          modify (\s -> s
+            { selectedRunner = Just runnerName
+            , statusText = "Selected runner: " <> runnerName
+            })
+
+providerModels :: Config -> Text -> [Text]
+providerModels cfg providerName =
+  case M.lookup providerName (providers cfg) of
+    Nothing -> []
+    Just Provider{chat_model_id, coder_model_id, reasoner_model_id} ->
+      List.nub [chat_model_id, coder_model_id, reasoner_model_id]
 
 app :: App AppState e Name
 app =
@@ -75,6 +149,7 @@ app =
     , appAttrMap = const (attrMap Vty.defAttr
       [ (L.listAttr, Vty.defAttr)
       , (L.listSelectedAttr, Vty.defAttr `Vty.withStyle` Vty.reverseVideo)
+      , (attrName "focusBorder", Vty.defAttr `Vty.withForeColor` Vty.cyan)
       ])
     }
 
@@ -83,15 +158,36 @@ main = do
   mPath <- findConfig
   case mPath of
     Nothing -> do
-      let st = mkState [] "providers.toml not found"
+      let emptyCfg = Config mempty (Defaults "" "") mempty
+      let st = mkState emptyCfg [] [] defaultRunners "providers.toml not found"
       void $ defaultMain app st
     Just path -> do
       cfgResult <- loadConfig path
       case cfgResult of
         Left err -> do
-          let st = mkState [] ("Failed to load config: " <> err)
+          let emptyCfg = Config mempty (Defaults "" "") mempty
+          let st = mkState emptyCfg [] [] defaultRunners ("Failed to load config: " <> err)
           void $ defaultMain app st
         Right cfg -> do
           let providerNames = sort (M.keys (providers cfg))
-          let st = mkState providerNames ("Loaded providers from " <> toText path)
+          let st = mkState cfg providerNames [] defaultRunners ("Loaded providers from " <> toText path)
           void $ defaultMain app st
+
+defaultRunners :: [Text]
+defaultRunners =
+  [ "basic"
+  , "chat"
+  , "embeddings"
+  , "structured"
+  , "tool-calling"
+  , "partial"
+  , "prefix"
+  , "fim"
+  , "get-models"
+  ]
+
+focusBorder :: Bool -> Widget Name -> Widget Name
+focusBorder isActive =
+  if isActive
+    then withAttr (attrName "focusBorder")
+    else id
--- tui/Oasis/Tui/State.hs
+++ tui/Oasis/Tui/State.hs
@@ -7,20 +7,37 @@ module Oasis.Tui.State
 import Relude
 import qualified Brick.Widgets.List as L
 import qualified Data.Vector as V
+import Oasis.Types (Config)
 
 
 data Name
   = ProviderList
+  | ModelList
+  | RunnerList
   deriving (Eq, Ord, Show)
 
 data AppState = AppState
-  { providerList :: L.List Name Text
+  { config :: Config
+  , providerList :: L.List Name Text
+  , modelList :: L.List Name Text
+  , runnerList :: L.List Name Text
+  , activeList :: Name
+  , selectedProvider :: Maybe Text
+  , selectedModel :: Maybe Text
+  , selectedRunner :: Maybe Text
   , statusText :: Text
   }
 
-mkState :: [Text] -> Text -> AppState
-mkState providers statusText =
+mkState :: Config -> [Text] -> [Text] -> [Text] -> Text -> AppState
+mkState cfg providers models runners statusText =
   AppState
-    { providerList = L.list ProviderList (V.fromList providers) 1
+    { config = cfg
+    , providerList = L.list ProviderList (V.fromList providers) 1
+    , modelList = L.list ModelList (V.fromList models) 1
+    , runnerList = L.list RunnerList (V.fromList runners) 1
+    , activeList = ProviderList
+    , selectedProvider = Nothing
+    , selectedModel = Nothing
+    , selectedRunner = Nothing
     , statusText
     }
